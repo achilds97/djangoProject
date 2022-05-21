91e3ffe8144d2cd79d2580e31693f143ebe5103f
Alex Childs <achilds97@outlook.com>
2022-05-21 12:09:40 -0600

    Added in some main functionality

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
+from django.views import generic
+from .models import Lead, Agent
+from .forms import LeadForm, LeadModelForm
 
-# Create your views here.
+
+class LandingPageView(generic.TemplateView):
+    template_name = "landing.html"
+
+
+class LeadListView(generic.ListView):
+    template_name = "leads/leads_list.html"
+    queryset = Lead.objects.all()
+    context_object_name = "leads"
+
+
+class LeadDetailView(generic.DetailView):
+    template_name = "leads/lead_detail.html"
+    queryset = Lead.objects.all()
+    context_object_name = "lead"
+
+
+class LeadCreateView(generic.CreateView):
+    template_name = "leads/lead_create.html"
+    form_class = LeadModelForm
+
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+
+class LeadUpdateView(generic.UpdateView):
+    template_name = "leads/lead_update.html"
+    form_class = LeadModelForm
+    queryset = Lead.objects.all()
+
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+
+class LeadDeleteView(generic.DeleteView):
+    template_name = "leads/lead_delete.html"
+    queryset = Lead.objects.all()
+
+    def get_success_url(self):
+        return reverse("leads:lead-list")
