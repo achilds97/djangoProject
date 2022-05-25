b842d4b9a3607d4501f310e52a4108e08260f50a
Alex Childs <achilds97@outlook.com>
2022-05-25 13:40:31 -0600

    Added in adding/updating agents as well as user restriction based on user type.

---
+++ agents/views.py
@@ -0,0 +1,75 @@
+import random
+from django.shortcuts import render, reverse
+from django.views import generic
+from django.core.mail import send_mail
+from django.contrib.auth.mixins import LoginRequiredMixin
+from leads.models import Agent
+from .forms import AgentModelForm
+from .mixins import OrganiserAndLoginRequiredMixin
+
+class AgentListView(OrganiserAndLoginRequiredMixin, generic.ListView):
+    template_name = "agents/agent_list.html"
+
+    def get_queryset(self):
+        organization = self.request.user.userprofile
+        return Agent.objects.filter(organization=organization)
+
+
+class AgentCreateView(OrganiserAndLoginRequiredMixin, generic.CreateView):
+    template_name = "agents/agent_create.html"
+
+    form_class = AgentModelForm
+
+    def get_success_url(self):
+        return reverse("agents:agent-list")
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_agent = True
+        user.is_organiser = False
+        user.set_password(f"{random.randint(0, 1000000)}")
+        user.save()
+        Agent.objects.create(
+            user=user,
+            organization=self.request.user.userprofile
+        )
+
+        send_mail(
+            subject="You are invited to be an agent",
+            message="You were added as an agent on DJCRM. Please come log in to start working.",
+            from_email="admin@test.com",
+            recipient_list=[user.email],
+        )
+
+        return super(AgentCreateView, self).form_valid(form)
+
+
+class AgentDetailView(OrganiserAndLoginRequiredMixin, generic.DetailView):
+    template_name = "agents/agent_detail.html"
+
+    def get_queryset(self):
+        organization = self.request.user.userprofile
+        return Agent.objects.filter(organization=organization)
+
+
+class AgentUpdateView(OrganiserAndLoginRequiredMixin, generic.UpdateView):
+    template_name = "agents/agent_update.html"
+    form_class = AgentModelForm
+
+    def get_queryset(self):
+        organization = self.request.user.userprofile
+        return Agent.objects.filter(organization=organization)
+
+    def get_success_url(self):
+        return reverse("agents:agent-list")
+
+
+class AgentDeleteView(OrganiserAndLoginRequiredMixin, generic.DeleteView):
+    template_name = "agents/agent_delete.html"
+
+    def get_queryset(self):
+        organization = self.request.user.userprofile
+        return Agent.objects.filter(organization=organization)
+
+    def get_success_url(self):
+        return reverse("agents:agent-list")
--- leads/views.py
+++ leads/views.py
@@ -1,47 +1,107 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
 from django.views import generic
+from django.core.mail import send_mail
 from .models import Lead, Agent
-from .forms import LeadForm, LeadModelForm
+from .forms import LeadForm, LeadModelForm, CustomUserCreationForm
+from agents.mixins import OrganiserAndLoginRequiredMixin
+
+
+class SignupView(generic.CreateView):
+    template_name = "registration/signup.html"
+    form_class =  CustomUserCreationForm
+
+    def get_success_url(self):
+        return reverse("login")
 
 
 class LandingPageView(generic.TemplateView):
     template_name = "landing.html"
 
 
-class LeadListView(generic.ListView):
+class LeadListView(LoginRequiredMixin, generic.ListView):
     template_name = "leads/leads_list.html"
-    queryset = Lead.objects.all()
     context_object_name = "leads"
 
+    def get_queryset(self):
+        user = self.request.user
 
-class LeadDetailView(generic.DetailView):
+        if user.is_organiser:
+            queryset = Lead.objects.filter(organization=user.userprofile, agent__isnull=False)
+        else:
+            queryset = Lead.objects.filter(organization=user.userprofile, agent__isnull=False)
+            queryset = queryset.filter(agent__user=user)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        user = self.request.user
+        context = super(LeadListView, self).get_context_data(**kwargs)
+
+        if user.is_organiser:
+            queryset = Lead.objects.filter(organization=user.userprofile, agent__isnull=True)
+            context.update({
+                "unassigned_leads": queryset
+            })
+
+        return context
+
+
+class LeadDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = "leads/lead_detail.html"
-    queryset = Lead.objects.all()
     context_object_name = "lead"
 
+    def get_queryset(self):
+        user = self.request.user
 
-class LeadCreateView(generic.CreateView):
+        if user.is_organiser:
+            queryset = Lead.objects.filter(organization=user.userprofile)
+        else:
+            queryset = Lead.objects.filter(organization=user.agent.organization)
+            queryset = queryset.filter(agent__user=user)
+
+
+class LeadCreateView(OrganiserAndLoginRequiredMixin, generic.CreateView):
     template_name = "leads/lead_create.html"
     form_class = LeadModelForm
 
     def get_success_url(self):
         return reverse("leads:lead-list")
 
+    def form_valid(self, form):
+        lead = form.save(commit=False)
+        lead.organization = self.request.user.userprofile
 
-class LeadUpdateView(generic.UpdateView):
+        send_mail(
+            subject="A lead has been created",
+            message="Go to the site to see the new lead",
+            from_email="test@test.com",
+            recipient_list=["test2@test.com"]
+        )
+        return super(LeadCreateView, self).form_valid(form)
+
+
+class LeadUpdateView(OrganiserAndLoginRequiredMixin, generic.UpdateView):
     template_name = "leads/lead_update.html"
     form_class = LeadModelForm
-    queryset = Lead.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Lead.objects.filter(organization=user.userprofile)
 
     def get_success_url(self):
         return reverse("leads:lead-list")
 
 
-class LeadDeleteView(generic.DeleteView):
+class LeadDeleteView(OrganiserAndLoginRequiredMixin, generic.DeleteView):
     template_name = "leads/lead_delete.html"
-    queryset = Lead.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Lead.objects.filter(organization=user.userprofile)
 
     def get_success_url(self):
         return reverse("leads:lead-list")
+
+
